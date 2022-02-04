05ad50ffc64aa356f3e6f2241eb13cb44cc53887
PJgvntofly <98489201+PJgvntofly@users.noreply.github.com>
2022-02-03 18:42:37 -0800

    added error conditions

--- get_recommendations.py
+++ get_recommendations.py
@@ -1,9 +1,18 @@
 from multiprocessing.connection import wait
 import recommendations
 
-call_type = input("Enter the call type:")
-grid = input("Enter the fire grid:")
+def get_recommendations():
+    call_type = input("Enter the call type:")
+    grid = input("Enter the fire grid:")
+    grid = grid.strip().upper()
+    call_type = call_type.strip().upper()
+    if recommendations.get_radio(grid):
+        if call_type in recommendations.TAC_1.keys():
+            print(recommendations.recommendations(call_type,grid),'\n')
+        else:
+            print(f"{call_type} is not valid. \nPlease enter a valid call type \n")
+    else:
+        print(f'{grid} is not valid. \nPlease enter a valid fire grid \n')
+    get_recommendations()
 
-print(recommendations.recommendations(call_type,grid))
-
-input('Hit enter to close')
+get_recommendations()
