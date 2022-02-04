9d3ba57711c875fecda29476be5df903f51b1c64
PJgvntofly <98489201+PJgvntofly@users.noreply.github.com>
2022-02-03 17:33:14 -0800

    bult basic recommendations app

--- get_recommendations.py
+++ get_recommendations.py
@@ -1,6 +1,9 @@
+from multiprocessing.connection import wait
 import recommendations
 
 call_type = input("Enter the call type:")
 grid = input("Enter the fire grid:")
 
 print(recommendations.recommendations(call_type,grid))
+
+input('Hit enter to close')
