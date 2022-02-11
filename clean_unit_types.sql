f9a51d541065efc56910766120458bad213a577c
PJgvntofly <98489201+PJgvntofly@users.noreply.github.com>
2022-02-10 17:01:31 -0800

    Bug fixes

--- clean_unit_types.sql
+++ clean_unit_types.sql
@@ -3,4 +3,4 @@ SET unit_type = (CASE WHEN unit_type LIKE "%Engine%" THEN "Engine"
 					  WHEN unit_type LIKE "%Aid Unit%" THEN "Aid Unit"
                       WHEN unit_type LIKE "%Ladder%" THEN "Ladder"
 				END)
-WHERE jurisdiction LIKE '31%' or jurisdiction LIKE '17%' AND (unit_type LIKE '%Engine%' OR unit_type LIKE '%Aid Unit%' OR unit_type LIKE '%Ladder%')
+WHERE (jurisdiction LIKE '31%' or jurisdiction LIKE '17%') AND (unit_type LIKE '%Engine%' OR unit_type LIKE '%Aid Unit%' OR unit_type LIKE '%Ladder%')
