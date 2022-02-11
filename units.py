4c89a5af10623c59e50031faaa8942585f1cd4c7
PJgvntofly <98489201+PJgvntofly@users.noreply.github.com>
2022-02-11 14:17:49 -0800

    removed duplicate error message, added xstaff

--- units.py
+++ units.py
@@ -4,13 +4,14 @@ import create_server_connection
 import csv
 
 class Unit:
-    def __init__(self, unit_number, unit_type, unit_station, unit_status):
+    def __init__(self, unit_number, unit_type, unit_station, unit_status, cross_staffing):
         self.unit_number = unit_number
         self.unit_type = unit_type
         self.unit_station = unit_station
         self.unit_status = unit_status
+        self.cross_staffing = cross_staffing
     def __str__(self):
-        return '\nUnit Number: ' + self.unit_number + '\nUnit Type: ' + self.unit_type + '\nAssigned Station: ' + self.unit_station + '\nUnit Status: ' + self.unit_status
+        return '\nUnit Number: ' + self.unit_number + '\nUnit Type: ' + self.unit_type + '\nAssigned Station: ' + self.unit_station + '\nUnit Status: ' + self.unit_status + '\nCross Staffing: ' +self.cross_staffing
 
 def create_connection():
     connection = create_server_connection.create_server_connection('localhost', 'recommendations','testpassword1','cad_lite')
@@ -25,7 +26,8 @@ SELECT
 unit_number,
 unit_type,
 assigned_station,
-unit_status
+unit_status,
+cross_staffing
 FROM
 units
 WHERE
@@ -37,19 +39,26 @@ def refresh_units():
     connection = create_connection()
     if connection != None:
         results = create_server_connection.read_query(connection, q1)
-        for unit_number, unit_type, assigned_station, unit_status in results:
-            unitx = Unit(unit_number, unit_type, assigned_station, unit_status)
+        for unit_number, unit_type, assigned_station, unit_status, cross_staffing in results:
+            unitx = Unit(unit_number, unit_type, assigned_station, unit_status, cross_staffing)
             unit_list.append(unitx)
             connection.close()
+            for unit in unit_list:
+                unit.cross_staffing = unit.cross_staffing.split('-')
     else:
-        print('Failed to connect to CAD Lite Database \nStarting Offline Mode')
-        f = open(r'C:\Users\cgass\Downloads\CADLiteUnitList_2021-04-16.csv','r', newline='')
-        csv_f = csv.reader(f)
-        for row in csv_f:
-            unit_number, jurisdiction, unit_type, assigned_station, assigned_beat, display_in_usm, unit_status, cross_staffing = row
-            unitx = Unit(unit_number, unit_type, assigned_station, unit_status)
-            unit_list.append(unitx)
-        f.close()
-        for unit in unit_list:
-            unit.unit_status = 'AIQ'
+        unit_list = import_units()
+    return unit_list
+
+def import_units():
+    unit_list = []
+    print('Failed to connect to CAD Lite Database \nStarting Offline Mode')
+    f = open(r'C:\Users\cgass\OneDrive\Documents\CAD Lite\CADLiteUnitList_2021-04-16.csv','r', newline='')
+    csv_f = csv.reader(f)
+    for row in csv_f:
+        unit_number, jurisdiction, unit_type, assigned_station, assigned_beat, display_in_usm, unit_status, cross_staffing = row
+        unitx = Unit(unit_number, unit_type, assigned_station, unit_status)
+        unit_list.append(unitx)
+    f.close()
+    for unit in unit_list:
+        unit.unit_status = 'AIQ'
     return unit_list
