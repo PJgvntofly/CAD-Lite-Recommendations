972ef7f4be49f9515f25efcc6d08b771088d6d2f
PJgvntofly <98489201+PJgvntofly@users.noreply.github.com>
2022-05-11 16:03:28 -0700

    loading gif

--- create_api_connection.py
+++ create_api_connection.py
@@ -1,6 +1,7 @@
 import requests
 from urllib3.exceptions import InsecureRequestWarning
 from log_config import connection_log
+import PySimpleGUI as sg
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
@@ -10,13 +11,18 @@ def create_api_connection(url, organization_id, jurisdiction):
         'organizationId':organization_id,
         'jurisdiction':jurisdiction
     }
-    try:
-        response = requests.get(url, params=parameters, verify=False)
-        connection_log.info(f"Verbose connection successful. Status code: {response.status_code}")
-        print("CAD Lite Database connection successful")
-    except:
-        connection_log.exception("")
-        print(f"Error connecting to CAD Lite Database")
+    while response == None:
+        sg.PopupAnimated(image_source='loading.gif',background_color=None)
+        try:
+            response = requests.get(url, params=parameters, verify=False)
+            connection_log.info(f"Verbose connection successful. Status code: {response.status_code}")
+            sg.PopupAnimated(image_source=None)
+            print("CAD Lite Database connection successful")
+        except:
+            connection_log.exception("")
+            sg.PopupAnimated(image_source=None)
+            print(f"Error connecting to CAD Lite Database")
+            response = False
     return response
 
 def silent_api_connection(url, organization_id, jurisdiction, verify=False):
