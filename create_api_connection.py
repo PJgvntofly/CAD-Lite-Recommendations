5a5224d4f30a887d184de71661dd26cf44947c17
Christopher Gass <98489201+PJgvntofly@users.noreply.github.com>
2023-12-14 08:53:53 -0800

    Keeps window on top. Adds code optimizations

--- create_api_connection.py
+++ create_api_connection.py
@@ -6,35 +6,32 @@ import PySimpleGUI as sg
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-def create_api_connection(url, organization_id, jurisdiction):
+#Creates a popup window roughly centered in the main window showing that the API connection is in process
+def create_loading_window(window):
+    main_window_location = window.current_location()
+    text = 'CAD Lite Recommendations Loading...'
+    popup_size = (len(text), 10)
+    popup_x = (main_window_location[0] + window.size[0] // 2) - popup_size[0] // 2
+    popup_y = (main_window_location[1] + window.size[1] // 2) - popup_size[1]
+
+    sg.popup_no_buttons(text, location=(popup_x, popup_y), no_titlebar=True,non_blocking=True,auto_close_duration=.1, auto_close=True, keep_on_top=True)
+
+#Creates an API connection with a popup showing that the connection is in process
+def create_api_connection(url, organization_id, jurisdiction, window):
     response = None
     parameters = {
         'organizationId':organization_id,
         'jurisdiction':jurisdiction
     }
     connection_log.info('Attempting API connection')
-    sg.popup_no_buttons('CAD Lite Recommendations Loading...',no_titlebar=True,non_blocking=True,auto_close_duration=4, auto_close=True)
+    create_loading_window(window)
+
     try:
         response = requests.get(url, params=parameters, verify=False)
         connection_log.info(f"Verbose connection successful. Status code: {response.status_code}")
         print("CAD Lite Database connection successful")
-    except:
-        connection_log.exception("")
-        sg.PopupAnimated(image_source=None)
+    except Exception as e:
+        connection_log.exception(f"An error occured while establishing the API connection - {e}")
         print(f"Error connecting to CAD Lite Database")
         response = False
-    return response
-
-def silent_api_connection(url, organization_id, jurisdiction, verify=False):
-    response = None
-    parameters = {
-        'organizationId':organization_id,
-        'jurisdiction':jurisdiction
-    }
-    try:
-        response = requests.get(url, params=parameters)
-        connection_log.info(f"Silent connection successful. Status code: {response.status_code}")
-    except:
-        connection_log.exception("")
-        print(f"Error connecting to CAD Lite Database")
     return response
