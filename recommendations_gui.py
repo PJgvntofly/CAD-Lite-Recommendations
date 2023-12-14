38013b294fbdfd04c9894e9bd2fcc28667c6c311
Christopher Gass <98489201+PJgvntofly@users.noreply.github.com>
2023-12-14 09:26:28 -0800

    Code optimizations

--- recommendations_gui.py
+++ recommendations_gui.py
@@ -2,69 +2,55 @@
 
 import PySimpleGUI as sg
 from get_recommendations import get_recommendations
-from import_response_plans import import_response_plans
 
 sg.theme('DarkBlack1')
 sg.set_global_icon('SNO911-Logo.ico')
 
-entry_mask = [
-    [
-        sg.Text("Call Type:"),
-        sg.Push(),sg.In(size=(15,1), key="-CALL TYPE-")
-    ],
-    [
-        sg.Text("Grid:"),
-        sg.Push(), sg.In(size=(15,1), key="-GRID-")
-    ],
-    [
-        sg.Button("Submit",bind_return_key=True), sg.Button("Clear")
+#Create etext boxes for users to enter the call type and grid plus a submit button
+def create_entry_mask():
+    return [
+        [sg.Text("Call Type:"), sg.Push(), sg.In(size=(15,1), key="-CALL TYPE-")],
+        [sg.Text("Grid:"), sg.Push(), sg.In(size=(15,1), key="-GRID-")],
+        [sg.Button("Submit",bind_return_key=True), sg.Button("Clear")]
     ]
-]
 
-return_viewer = [
-    [
-        sg.Text('Recommendation:')
-    ],
-    [
-        sg.Output(size=(50,5), key='-OUTPUT-')
-    ],
-    [
-        sg.Push(), sg.Button("Exit")
+#Create window for viewing output
+def create_return_viewer():
+    return [
+        [sg.Text('Recommendation:')],
+        [sg.Output(size=(50,5), key='-OUTPUT-')],
+        [sg.Push(), sg.Button("Exit")]
     ]
-]
 
-layout = [
-    [
-        sg.Titlebar(title='CAD Lite Recommendations')
-    ],
-    [
-        sg.Column(entry_mask),
-        sg.VSeperator(),
-        sg.Column(return_viewer)
+def main():
+    entry_mask = create_entry_mask()
+    return_viewer = create_return_viewer()
+    layout = [
+        [sg.Titlebar(title='CAD Lite Recommendations')],
+        [sg.Column(entry_mask), sg.VSeperator(), sg.Column(return_viewer)]
     ]
-]
 
-window = sg.Window("CAD Lite Recommendations", layout,font='Courier 14')
+    window = sg.Window("CAD Lite Recommendations", layout, no_titlebar=False, finalize=True, font='Courier 14', grab_anywhere=True, keep_on_top=True)
 
-gif = sg.Image(source='loading.gif')
-win2_active = False
-while True:
-    event, values = window.read()
-    response_plans = import_response_plans()
-    if event == sg.WIN_CLOSED or event == "Exit":
-        break
-    if event == "Submit":
-        call_type = values["-CALL TYPE-"]
-        grid = values["-GRID-"]
-        #print(f'Entered Call Type: {call_type.upper()}\nEntered Grid: {grid.upper()}')
-        recommendation = get_recommendations(call_type, grid)
-        print(recommendation)
-        window['-CALL TYPE-'].update("")
-        window['-GRID-'].update("")
-        window['-CALL TYPE-'].set_focus()
-    if event == "Clear":
-        window['-CALL TYPE-'].update("")
-        window['-GRID-'].update("")
-        window['-OUTPUT-'].update("")
-        window['-CALL TYPE-'].set_focus()
-window.close()
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED or event == "Exit":
+            break
+        if event == "Submit":
+            call_type = values["-CALL TYPE-"]
+            grid = values["-GRID-"]
+            recommendation = get_recommendations(call_type, grid, window)
+            print(recommendation)
+            window['-CALL TYPE-'].update("")
+            window['-GRID-'].update("")
+            window['-CALL TYPE-'].set_focus()
+        if event == "Clear":
+            window['-CALL TYPE-'].update("")
+            window['-GRID-'].update("")
+            window['-OUTPUT-'].update("")
+            window['-CALL TYPE-'].set_focus()
+    window.close()
+
+if __name__ == "__main__":
+    main()
