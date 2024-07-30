a6fb5de0fc819deb2002ff4fb6f463a0817fcfd5
Christopher Gass <98489201+PJgvntofly@users.noreply.github.com>
2024-07-30 14:06:57 -0700

    Adds response plans for new train call tyes

---
+++ test_import_response_plans.py
@@ -0,0 +1,38 @@
+from import_response_plans import import_response_plans, update_response_plan
+import unittest
+
+class TestResponsePlans(unittest.TestCase):
+    def test_tac_1(self):
+        data = import_response_plans()
+        self.assertEqual(data['SNO911']['TAC-1']['BLS1'], [["Aid Unit", "Engine", "Ladder"]])
+    
+    def test_31D01_rfa(self):
+        data = import_response_plans()
+        self.assertEqual(data['SNO911']['31D01_rfa']['BLS1'], [['Aid Unit', 'Engine', 'Ladder']])
+    
+    def test_create_new_agency(self):
+        update_response_plan('ValleyCOM', 'Test_Zone', 'Test', ['Engine'])
+        data = import_response_plans()
+        self.assertEqual(data['VALLEYCOM']['TEST_ZONE']['TEST'], ['Engine'])
+    
+    def test_create_new_zone(self):
+        update_response_plan('ValleyCOM', 'Test_Zone2', 'Test', ['Engine'])
+        data = import_response_plans()
+        self.assertEqual(data['VALLEYCOM']['TEST_ZONE']['TEST'], ['Engine'])
+        self.assertEqual(data['VALLEYCOM']['TEST_ZONE2']['TEST'], ['Engine'])
+    
+    def test_add_plan_to_zone(self):
+        update_response_plan('ValleyCom', 'Test_zone', 'test2', ['Engine', 'Aid Unit'])
+        data = import_response_plans()
+        self.assertEqual(data['VALLEYCOM']['TEST_ZONE']['TEST2'], ['Engine', 'Aid Unit'])
+    
+    def test_update_existing_plan(self):
+        update_response_plan('ValleyCOM', 'Test_Zone', 'Test2', ['Engine', ['Aid Unit', 'Medic Unit']])
+        data = import_response_plans()
+        self.assertEqual(data['VALLEYCOM']['TEST_ZONE']['TEST'], ['Engine'])
+        self.assertEqual(data['VALLEYCOM']['TEST_ZONE']['TEST2'], ['Engine', ['Aid Unit', 'Medic Unit']])
+        self.assertEqual(data['SNO911']['TAC-1']['BLS1'], [["Aid Unit", "Engine", "Ladder"]])
+        
+
+if __name__ == '__main__':
+    unittest.main()
